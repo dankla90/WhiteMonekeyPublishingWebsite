f866be8c8b1f78bea698b91566b853e9d1a9247f
dankla90 <bassboom@hotmail.com>
2024-06-24 19:12:08 +0200

    added links to images

--- whitemonkeypublishing/src/pages/Home.jsx
+++ whitemonkeypublishing/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../App.css'; // Ensure this import is here to apply the styles
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate('/books');
+  };
+
   return (
     <div className="home">
       <div className="text">
@@ -15,9 +22,19 @@ function Home() {
       </div>
 
       <div className="images">
-        <img src={process.env.PUBLIC_URL + '/img/Home1.jpg'} alt="Arbie & Fizzie book characters" />
+        <img 
+          src={process.env.PUBLIC_URL + '/img/Home1.jpg'} 
+          alt="Arbie & Fizzie book characters" 
+          onClick={handleImageClick} 
+          style={{ cursor: 'pointer' }} // Change cursor to pointer to indicate it's clickable
+        />
 
-        <img src={process.env.PUBLIC_URL + '/img/Cover.jpg'} alt="cover of is that really how it is book" />
+        <img 
+          src={process.env.PUBLIC_URL + '/img/Cover.jpg'} 
+          alt="cover of is that really how it is book" 
+          onClick={handleImageClick} 
+          style={{ cursor: 'pointer' }} // Change cursor to pointer to indicate it's clickable
+        />
       </div>
     </div>
   );
